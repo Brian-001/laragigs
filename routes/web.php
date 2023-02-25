d383082ad4a04fba63030e9b10532264083e9b88
Brian-001 <karanjabrian001@gmail.com>
2023-02-25 23:59:08 +0300

    Passing content

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'latest listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Architecto, iure eaque nostrum vero aspernatur excepturi! 
+                Possimus rem accusamus reiciendis non fugit ipsum, ea 
+                expedita dolorem totam provident enim ullam doloribus.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Architecto, iure eaque nostrum vero aspernatur excepturi! 
+                Possimus rem accusamus reiciendis non fugit ipsum, ea 
+                expedita dolorem totam provident enim ullam doloribus.'
+            ]
+        ] 
+    ]);
 });
