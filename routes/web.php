fa7a1962c5cc01b9c462b5778ae859d1c5279e21
Brian-001 <karanjabrian001@gmail.com>
2023-03-01 23:37:18 +0300

    Modiified Model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,26 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//All listings
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'latest listings',
-        'listings' => [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Architecto, iure eaque nostrum vero aspernatur excepturi! 
-                Possimus rem accusamus reiciendis non fugit ipsum, ea 
-                expedita dolorem totam provident enim ullam doloribus.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Architecto, iure eaque nostrum vero aspernatur excepturi! 
-                Possimus rem accusamus reiciendis non fugit ipsum, ea 
-                expedita dolorem totam provident enim ullam doloribus.'
-            ]
-        ] 
+        'listings' => Listing::all()
     ]);
 });
+
+
+//Single Listings
+
