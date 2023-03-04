946c2494878ab60e57b88879e3c02ef49b71f7cc
Brian-001 <karanjabrian001@gmail.com>
2023-03-04 23:27:32 +0300

    created Listing Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //  \App\Models\User::factory(5)->create();
+         \App\Models\User::factory(6)->create();
 
         Listing::create([
             'title'=>'Laravel Senior Developer',
