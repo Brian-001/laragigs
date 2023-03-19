e52a9d23b5020c43e2de3510a229b02682b1b7b6
Brian-001 <karanjabrian001@gmail.com>
2023-03-19 10:03:59 +0300

    modified listing.blade.php and layout folder

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -3,4 +3,18 @@
 @section('content') 
     
     <a href="{{url('/listings.blade.php')}}" class="inline-block text-black ml-4 mb-4">Back</a>
+    <div class="mx-4">
+       <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+            <div class="flex flex-col items-center justify-center text-center">
+                <img src="images/acme.jpg" alt="" class="w-48 mr-6 mb-6">
+                <h3 class="text-2xl mb-2">Senior Laravel Developer</h3>
+                <div class="text-xl font-bold mb-4">Acme Corp</div>
+                <ul class="flex">
+                    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+                        <a href="#">Laravel</a>
+                    </li>
+                </ul>
+            </div>
+        </div> 
+    </div>
 @endsection
